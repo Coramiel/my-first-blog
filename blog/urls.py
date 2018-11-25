fde487bfe573a2ab5d99fc8f0abc9e3cbda6c833
Coralie <cobrechet@gmail.com>
2018-11-25 10:57:12 +0100

    Fix problem

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 from . import views
+from django.views.generic import RedirectView
 
 urlpatterns = [
     url(r'^$', views.post_list, name= 'post_list'),
+    url(r'^favicon\.ico$', RedirectView.as_view(url='/static/images/favicon.ico')),
 ]
 
